1fb9a8f047648c9a7ad670792c2948c6676a857a
Anant Rajeev <anant.rajeev@gmail.com>
2020-05-04 20:37:40 -0700

    Added in 5/4 creation

--- xemelgo-app/ddb_putitem.js
+++ xemelgo-app/ddb_putitem.js
@@ -8,8 +8,18 @@ var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 var params = {
     TableName: 'Employee',
     Item: {
-      'EmployeeID' : {N: '1'},
-      'EmployeeName' : {S: 'John Doe'}
+     'EmployeeID': {N: '1'},
+     'EmployeeFirstName': {S:'Hattie'},
+     'EmployeeLastName': {S:'Girling'},
+     'EmployeeAddress': {S:'017 Bellgrove Crossing'},
+     'EmployeeState': {S:'MN'},
+     'EmployeeZipCode': {S:'55108'},
+     'EmployeePhoneNumber': {S:'612-511-3968'},
+     'EmployeeEmail': {S:'hgirling0@indiatimes.com'},
+     'EmployeeBirthDate': {S:'01/30/1972'},
+     'EmployeePositionID': {N:'1'},
+     'EmployeeStartDate': {S:'04/10/1946'},
+     'EmployeeEndDate': {S:'05/23/1961'}
     }
   };
 
